6f60fedc215c5286d0342f6d84bab30e7eac6bbe
Liam Campbell <liam@muledesign.com>
2014-11-11 15:35:17 -0800

    Make sure assets are migrated from source to build

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    // Watch for changes; perform relevant task
     watch: {
       sass: {
         files: ['src/assets/css/*.scss'],
@@ -15,8 +17,18 @@ module.exports = function(grunt) {
           'src/*.mustache'
         ],
         tasks: ['mustache_html']
+      },
+      copy: {
+        files: [
+          'src/assets/img/*',
+          'src/assets/fonts/*',
+          'src/assets/js/*'
+        ],
+        tasks: ['copy']
       }
     },
+
+    // Compile Sass into CSS
     sass: {
       dist: {
         files: [{
@@ -28,16 +40,46 @@ module.exports = function(grunt) {
         }]
       }
     },
+
+    // Compile mustache templates into flat HTML
     mustache_html: {
       options: {
         src: 'src',
         dist: 'build',
         type: 'mustache'
       }
+    },
+
+    // Copy images, fonts, and javascript from src to build
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/assets/img',
+            src: ['**', '**/*'],
+            dest: 'build/assets/img/'
+          },
+          {
+            expand: true,
+            cwd: 'src/assets/fonts',
+            src: ['**', '**/*'],
+            dest: 'build/assets/fonts/'
+          },
+          {
+            expand: true,
+            cwd: 'src/assets/js',
+            src: ['**', '**/*'],
+            dest: 'build/assets/js/'
+          }
+        ]
+      }
     }
+
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-mustache-html');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 };
